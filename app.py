74d31b5645d37521a68d40a04f0ba7480ed7bb3d
sola <gksthf070@gmail.com>
2018-11-09 13:34:20 +0900

    Add : login
    로그인, 회원가입, 로그아웃 기능 추가.

--- app.py
+++ app.py
@@ -1,24 +1,28 @@
 #import all
 from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy #데이터베이스
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
-# from flask_bootstrap import Bootstrap # 이건 일단 보류
-from wtforms import StringField, PasswordField, BooleanField
+from flask_bootstrap import Bootstrap # 이건 일단 보류
+from wtforms import StringField, PasswordField, BooleanField #로그인 폼
 from wtforms.validators import InputRequired, Length, Email
 from flask_wtf import FlaskForm 
+from werkzeug.security import generate_password_hash, check_password_hash #비밀번호 보안문제
 
 app = Flask(__name__)
 
 #데이터베이스 관리
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/sola/Desktop/bookrental/booklist.db'
-app.config['SECRET_KEY'] = 'thisissecret'
+app.config['SECRET_KEY'] = 'thisissecret' #로그인을 넣으려면 꼭 필요하다.
+
+bootstrap = Bootstrap(app) #..보류
 
 db = SQLAlchemy(app)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
+login_manager.login_view = 'login'
 
-#도서 데이터베이스
+#도서 데이터베이스 칼럼 ... 나중에 이미지 파일 추가
 class Booklist(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
@@ -27,7 +31,7 @@ class Booklist(db.Model):
     status = db.Column(db.String(50))
     date = db.Column(db.String(50))
 
-#사용자 데이터베이스
+#사용자 데이터베이스 칼럼 ... 슬랙과 연동하는 문제 추후 해결
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50),unique=True)
@@ -39,36 +43,54 @@ class User(UserMixin, db.Model):
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-class LoginForm(FlaskForm): #로그인폼
+class LoginForm(FlaskForm): #로그인 화면 구성
     username = StringField('username', validators=[InputRequired(), Length(min=3, max=15)])
     password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
     remember = BooleanField('remember me')
 
-@app.route('/login', methods=['GET','POST'])#로그인
-def login():
-    
-    form = LoginForm(FlaskForm)
+class RegisterForm(FlaskForm): #가입 화면 구성
+    email = StringField('email', validators=[InputRequired(), Email(message='Invaild email'), Length(max=50)])
+    username = StringField('username', validators=[InputRequired(), Length(min=3, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
 
-    if form.validators_on_submit():
+@app.route('/login2', methods=['GET','POST'])#로그인 페이지
+def login2():
+    form = LoginForm()
+    
+    if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user:
-            if password(user.password, form.password.data):
+            if check_password_hash(user.password, form.password.data):
                 login_user(user, remember=form.remember.data)
-                return redirect(url_for('mypage'))
+                return redirect(url_for('index'))
         return '이름이나 비밀번호를 다시 확인해 주세요'
-    return render_template('login.html')
+    return render_template('login2.html', form=form)
+
+@app.route('/signup', methods=['GET', 'POST'])#가입페이지
+def singup():
+    
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        hashed_password = generate_password_hash(form.password.data, method='sha256')
+        new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(url_for('login2'))
+
+    return render_template('signup.html', form=form)
 
 @app.route('/logout')#로그아웃
+@login_required
 def logout():
     logout_user()
-    return redirect(url_for('index'))
-
-
+    return redirect(url_for('login2'))
 
 #page route
 @app.route('/')#첫 페이지
 def index():
-    return render_template('index.html')
+    return render_template('index.html', name=current_user.username)
 
 @app.route('/mypage')#마이페이지
 @login_required ##로그인한 사용자만 들어갈 수 있게 설정
